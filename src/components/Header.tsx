93e0cad17c70e13ac5f3902c18c04ad8ed23b40c
Park Min Hyeok <pmh_only@pmh.codes>
2021-12-25 23:52:56 +0900

    feat: add opengrahp
    Signed-off-by: Park Min Hyeok <pmh_only@pmh.codes>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,6 +6,13 @@ export default function Header () {
     <Helmet>
       <title>Subdomain Registrar - .gbsw.hs.kr 서브도메인 &amp; SSL 인증서 연결</title>
       <link rel="shortcut icon" href="favicon.png" type="image/png" />
+
+      <meta property="og:title" content="Subdomain Registrar" />
+      <meta property="og:site_name" content="Subdomain Registrar" />
+      <meta property="og:url" content="https://dns.gbsw.hs.kr" />
+      <meta property="og:description" content=".gbsw.hs.kr 서브도메인 &amp; SSL 인증서 연결" />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content="https://dns.gbsw.hs.kr/favicon.png" />
     </Helmet>
   )
 }
